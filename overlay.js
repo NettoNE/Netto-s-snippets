d1cc30a1ba9db5eb39f8320aea6390d9444afef0
Netto <netto@example.com>
2026-07-20 17:35:03 -0300

    add: componente de overlay em js vanilla

---
+++ overlay.js
@@ -0,0 +1,38 @@
+/*
+	overlay.js v0.1
+
+	Usage:
+	overlay(); or overlay({ remove: false });
+
+	Remove:
+	overlay({ remove: true });
+*/
+
+// CONSTs
+const Z_INDEX = 9998;
+const OVERLAY_CLASS_NAME = "js-overlay";
+
+export function overlay({ remove } = {}) {
+	let overlayElement = document.querySelector(`.${OVERLAY_CLASS_NAME}`);
+
+	if (remove) {
+		if (overlayElement) overlayElement.remove();
+		return;
+	}
+
+	if (!overlayElement) {
+		overlayElement = document.createElement("div");
+		overlayElement.classList.add(OVERLAY_CLASS_NAME);
+
+		Object.assign(overlayElement.style, {
+			position: "fixed",
+			inset: "0",
+			backgroundColor: "rgba(0,0,0,.5)",
+			zIndex: Z_INDEX
+		});
+
+		document.body.appendChild(overlayElement);
+	}
+
+	return overlayElement;
+}
